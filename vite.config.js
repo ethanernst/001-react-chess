b3a8a7a7de23eb91b8586ca13ff3a40fe885d22b
ethanernst <ethanernst1@gmail.com>
2023-10-30 22:11:59 -0400

    updated gh-pages config for new repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-chess',
+  base: '/001-react-chess',
   plugins: [react()],
 });
